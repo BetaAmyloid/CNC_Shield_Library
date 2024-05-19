433bfdf8891aa3b59f6f0bc4db8f57ec16ab2fb9
BetaAmyloid <132898202+BetaAmyloid@users.noreply.github.com>
2024-05-19 16:36:14 +0800

    Examples are added.
    Axis initialization is created.

---
+++ examples/Axis_Initialization/Axis_Initialization.ino
@@ -0,0 +1,30 @@
+#include <CNC_Shield.h>
+#define CW 1  // Clockwise
+#define CCW 0 // Counter-Clockwise
+
+CNC_Shield stepper1('X'); // Use Uppercase characters only
+CNC_Shield stepper2('Y');
+CNC_Shield stepper3('Z');
+CNC_Shield stepper4('A'); // A-axis does not a limit switch pin
+
+void setup()
+{
+    stepper1.begin();
+    stepper2.begin();
+    stepper3.begin();
+    stepper4.begin();
+}
+
+void loop()
+{
+    stepper1.step(200, CW);
+    stepper2.step(400, CW);
+    stepper3.step(800, CW);
+    stepper4.step(1600, CW);
+    delay(1000);
+    stepper1.step(200, CCW);
+    stepper2.step(400, CCW);
+    stepper3.step(800, CCW);
+    stepper4.step(1600, CCW);
+    delay(1000);
+}
